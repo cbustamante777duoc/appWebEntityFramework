9c3340151bb70eaeb84570a8f544a7c6a369f3e5
cbustamante777duoc <cra.bustamante@alumnos.duoc.cl>
2021-05-11 23:21:03 -0400

    Mi primera lista

---
+++ appWebEntityFramework/Controllers/MarcaController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using appWebEntityFramework.Models;
+
+namespace appWebEntityFramework.Controllers
+{
+    public class MarcaController : Controller
+    {
+        // GET: Marca
+        public ActionResult Index()
+        {
+            List<MarcaCLS> listaMarca = null;
+            using (var bd = new BDPasajeEntities())
+            {
+                 listaMarca = (from marca in bd.Marca
+                                             select new MarcaCLS
+                                             {
+                                                 iidmarca = marca.IIDMARCA,
+                                                 nombre = marca.NOMBRE,
+                                                 descripcion = marca.DESCRIPCION
+                                             }).ToList();
+            }
+
+            return View(listaMarca);
+        }
+    }
+}
---
+++ appWebEntityFramework/Models/MarcaCLS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace appWebEntityFramework.Models
+{
+    public class MarcaCLS
+    {
+        [Display(Name ="Id marca")]
+        public int iidmarca { get; set; }
+
+        [Display(Name = "nombre marca")]
+        public string nombre{ get; set; }
+
+        [Display(Name = "descripcion marca")]
+        public string descripcion { get; set; }
+
+        public int bhabilitado { get; set; }
+    }
+}
