75a390aa2403d902128c7648bf964ef5de5e4e5f
cbustamante777duoc <cra.bustamante@alumnos.duoc.cl>
2021-05-17 22:51:47 -0400

    cliente agregar done

--- appWebEntityFramework/Controllers/ClienteController.cs
+++ appWebEntityFramework/Controllers/ClienteController.cs
@@ -31,5 +31,69 @@ namespace appWebEntityFramework.Controllers
 
             return View(listaCliente);
         }
+
+
+        List<SelectListItem> listaSexo;
+
+        private void llenarSexo() 
+        {
+            using (var bd = new BDPasajeEntities())
+            {
+                listaSexo = (from sexo in bd.Sexo
+                             where sexo.BHABILITADO == 1
+                             select new SelectListItem
+                             {
+                                 Text = sexo.NOMBRE,
+                                 Value = sexo.IIDSEXO.ToString()
+
+                             }).ToList();
+
+                listaSexo.Insert(0, new SelectListItem { Text = "-----seleccione----", Value = "" });
+
+            }
+        
+        }
+
+        public ActionResult Agregar()
+        {
+            llenarSexo();
+            ViewBag.lista = listaSexo;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Agregar(ClienteCLS oClienteCLS) 
+        {
+            if (!ModelState.IsValid)
+            {
+                llenarSexo();
+                ViewBag.lista = listaSexo;
+
+                return View(oClienteCLS);
+            }
+
+            using (var bd = new BDPasajeEntities())
+            {
+                Cliente cliente = new Cliente();
+                cliente.NOMBRE = oClienteCLS.nombre;
+                cliente.APPATERNO = oClienteCLS.apMaterno;
+                cliente.APMATERNO = oClienteCLS.apMaterno;
+                cliente.EMAIL = oClienteCLS.email;
+                cliente.DIRECCION = oClienteCLS.direccion;
+                cliente.TELEFONOFIJO = oClienteCLS.telefonoFijo;
+                cliente.TELEFONOCELULAR = oClienteCLS.telefonoCelular;
+                cliente.IIDSEXO = oClienteCLS.iidsexo;
+                cliente.BHABILITADO = 1;
+                bd.Cliente.Add(cliente);
+                bd.SaveChanges();
+
+
+            }
+
+            return RedirectToAction("Index");
+        }
     }
+
+
 }
--- appWebEntityFramework/Models/ClienteCLS.cs
+++ appWebEntityFramework/Models/ClienteCLS.cs
@@ -12,23 +12,43 @@ namespace appWebEntityFramework.Models
         public int iidcliente { get; set; }
 
         [Display(Name = "Nombre cliente")]
+        [StringLength(100,ErrorMessage ="longuitud maxima de caracteres es de 100")]
+        [Required]
         public string nombre { get; set; }
 
         [Display(Name = "Apellido paterno ")]
+        [StringLength(150, ErrorMessage = "longuitud maxima de caracteres es de 150")]
+        [Required]
         public string apPaterno { get; set; }
 
         [Display(Name = "Apellido materno")]
+        [StringLength(150, ErrorMessage = "longuitud maxima de caracteres es de 150")]
+        [Required]
         public string apMaterno { get; set; }
 
+        [Display(Name = "Email ")]
+        [StringLength(200, ErrorMessage = "longuitud maxima de caracteres es de 200")]
+        [EmailAddress(ErrorMessage ="Ingrese un email valido")]
+        [Required]
         public string email { get; set; }
 
+        [Display(Name = "Direccion")]
+        [StringLength(200, ErrorMessage = "longuitud maxima de caracteres es de 200")]
+        [Required]
         public string direccion { get; set; }
 
+        [Display(Name = "Sexo ")]
+        [Required]
         public int iidsexo { get; set; }
 
         [Display(Name = "telefono fijo")]
+        [StringLength(10, ErrorMessage = "longuitud maxima de caracteres es de 10")]
+        [Required]
         public string telefonoFijo { get; set; }
 
+        [Display(Name = "telefono celular")]
+        [StringLength(10, ErrorMessage = "longuitud maxima de caracteres es de 10")]
+        [Required]
         public string telefonoCelular { get; set; }
 
         public int bhabilitado { get; set; }
