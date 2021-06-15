81f21789371dcd3138a95dccffeef9f9d35c7abc
cbustamante777duoc <cra.bustamante@alumnos.duoc.cl>
2021-06-14 23:35:29 -0400

    tipo usuario creado con la lista

---
+++ appWebEntityFramework/Controllers/TipoUsuarioController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using appWebEntityFramework.Models;
+
+namespace appWebEntityFramework.Controllers
+{
+    public class TipoUsuarioController : Controller
+    {
+        // GET: TipoUsuario
+        public ActionResult Index()
+        {
+            List<TipoUsuarioCLS> listaTipoUsuario = null;
+
+            using (var bd = new BDPasajeEntities())
+            {
+                listaTipoUsuario = (from tipoUsuario in bd.TipoUsuario
+                                    where tipoUsuario.BHABILITADO == 1
+                                    select new TipoUsuarioCLS
+                                    {
+                                        iidTipousuario = tipoUsuario.IIDTIPOUSUARIO,
+                                        nombre = tipoUsuario.NOMBRE,
+                                        descripcion = tipoUsuario.DESCRIPCION
+                                    }).ToList();
+
+            }
+
+            return View(listaTipoUsuario);
+        }
+    }
+}
---
+++ appWebEntityFramework/Models/TipoUsuarioCLS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace appWebEntityFramework.Models
+{
+    public class TipoUsuarioCLS
+    {
+        [Display(Name ="id tipo usuario")]
+        public int iidTipousuario { get; set; }
+
+        [Display(Name = "tipo nombre usuario")]
+        [Required]
+        [StringLength(150,ErrorMessage ="Longuitud maxima 150")]
+        public string nombre { get; set; }
+
+        [Display(Name = "Descripcion")]
+        [Required]
+        [StringLength(250, ErrorMessage = "Longuitud maxima 250")]
+        public string descripcion { get; set; }
+
+        public int bhabilitado { get; set; }
+    }
+}
