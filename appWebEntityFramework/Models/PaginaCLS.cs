5a1ba272317bae5b0cbea18f5c6bd2bc1a0c5b2b
cbustamante777duoc <cra.bustamante@alumnos.duoc.cl>
2021-06-16 23:47:35 -0400

    agregar nuevos controladores y vista parciales con ajax

---
+++ appWebEntityFramework/Models/PaginaCLS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace appWebEntityFramework.Models
+{
+    public class PaginaCLS
+    {
+        [Display(Name ="Id pagina")]
+        public int iidpagina { get; set; }
+
+        [Required]
+        [Display(Name = "Titulo del link")]
+        public string mensaje { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre de la accion")]
+        public string accion { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre del controlador")]
+        public string controlador { get; set; }
+        public int bhabilitado { get; set; }
+
+
+    }
+}
---
+++ appWebEntityFramework/Models/RolCLS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace appWebEntityFramework.Models
+{
+    public class RolCLS
+    {
+        [Display(Name ="Id rol")]
+        public int iidRol { get; set; }
+        [Required]
+        [Display(Name = "Nombre rol")]
+        public string nombre { get; set; }
+        [Required]
+        [Display(Name = "Descripcion rol")]
+        public string descripcion { get; set; }
+        public int bhabilitado { get; set; }
+
+
+    }
+}
